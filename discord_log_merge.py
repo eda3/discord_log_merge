d47b509eacf086ffb5f42d00e2bbac2c5726306e
eda3 <gerorin1010@gmail.com>
2019-05-13 18:22:40 +0900

    fix: change Line feed code

--- discord_log_merge.py
+++ discord_log_merge.py
@@ -8,7 +8,29 @@ def main():
 
         print('files', files)
         for f in files:
-            print(f)
+            # ファイル名からチャンネル名を抜き取り
+            channel_name = cut_out_channel_name(f)
+
+            print(channel_name)
+
+
+def cut_out_channel_name(file_name: str):
+    ''' Discordログのファイル名から、テキストチャンネル名を切り出しする
+    Args:
+        file_name(str): Discordログのファイル名
+    Returns:
+         channel_name(str): チャンネル名
+    '''
+
+    import re
+
+    pattern = r'.* - (.*) \[.*'
+    result = re.match(pattern, file_name)
+
+    if result:
+        channel_name = result.group(1)
+
+    return channel_name
 
 
 if __name__ == '__main__':
