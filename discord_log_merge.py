4730cc324977a44b9b92b7015b4d99aa4edba7fc
eda3 <gerorin1010@gmail.com>
2019-05-13 22:13:11 +0900

    add: chat_data list

--- discord_log_merge.py
+++ discord_log_merge.py
@@ -13,7 +13,10 @@ def main():
 
             file_path = work_dir + os.sep + f
 
-            file_reading(file_path)
+            chat_data = []
+
+            # 各ファイルを読み込み
+            chat_log_merge(file_path, channel_name)
 
 
 def cut_out_channel_name(file_name: str):
@@ -21,7 +24,7 @@ def cut_out_channel_name(file_name: str):
     Args:
         file_name(str): Discordログのファイル名
     Returns:
-         channel_name(str): チャンネル名
+     channel_name(str): チャンネル名
     '''
 
     import re
@@ -35,8 +38,9 @@ def cut_out_channel_name(file_name: str):
     return channel_name
 
 
-def file_reading(file_path: str):
+def chat_log_merge(file_path: str, channel_name: str):
     from bs4 import BeautifulSoup
+    import pprint
 
     chat_data = []
 
@@ -45,9 +49,15 @@ def file_reading(file_path: str):
         html = f.read()
         soup = BeautifulSoup(html, 'html.parser')
 
-    chat_text = soup.find_all('div', class_='chatlog__message-group')[0]
-    chat_timestamp = soup.find_all(
-        'span', class_='chatlog__timestamp')[0].string
+    chat_text_list = soup.find_all('div', class_='chatlog__message-group')
+    chat_timestamp_list = soup.find_all('span', class_='chatlog__timestamp')
+
+    # タイムスタンプ、チャット内容、チャンネル名をそれぞれリストに格納
+    for chat_text, chat_timestamp in zip(chat_text_list, chat_timestamp_list):
+        chat_data.append([chat_timestamp.string, chat_text, channel_name])
+
+    print('print')
+    pprint.pprint(chat_data[0])
 
 
 if __name__ == '__main__':
